01e0ee55577475f31124c51ba3f043b6740d3403
christianhelms <helmsmedia@gmail.com>
2022-03-05 14:55:53 -0600

    padding

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,16 @@ export default function Home() {
         <title>Front-End Mentor Practice Problem</title>
       </Head>
       <div className='grid place-items-center h-screen bg-dark-blue'>
-        <div className='bg-darkish-blue w-64	h-96 rounded-md p-4'>
+        <div className='bg-darkish-blue w-96	w-2/3 rounded-md p-6'>
           <Image 
             className='rounded-md'
             src={'/image-equilibrium.jpg'}
-            width={300}
-            height={300}
+            width={400}
+            height={400}
           />
-          <p className='text-white font-outfit font-semibold text-2xl'>Equilibrium #3429</p>
-          <p className='text-dark-gray font-outfit text-lg font-light'>Our Equilibrium collection promotes balance and calm.</p>
-          <div className='flex flex-row'>
+          <p className='text-white font-outfit font-semibold text-2xl pb-2 pt-2'>Equilibrium #3429</p>
+          <p className='text-dark-gray font-outfit text-lg font-light pb-2'>Our Equilibrium collection promotes balance and calm.</p>
+          <div className='flex flex-row border-b-2 border-very-dark-gray pb-4'>
             <div className='flex flex-row items-center'>
             <img 
               className='relitive pr-1'
