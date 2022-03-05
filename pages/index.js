a2572ea5f08e5974ed546a0edb730eafbcfaa3ed
christianhelms <helmsmedia@gmail.com>
2022-03-04 23:28:55 -0600

    added white in tailwinds config since it bugged. Added body text

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,11 @@ export default function Home() {
         <title>Front-End Mentor Practice Problem</title>
       </Head>
       <div className='grid place-items-center h-screen bg-dark-blue'>
-        <p className='text-white font-outfit font-semibold text-2xl'>Equilibrium #3429</p>
+        <div>
+          <Image />
+          <p className='text-white font-outfit font-semibold text-2xl'>Equilibrium #3429</p>
+          <p className='text-dark-gray font-outfit text-lg font-light'>Our Equilibrium collection promotes balance and calm.</p>
+        </div>
       </div>
     </main>
   )
