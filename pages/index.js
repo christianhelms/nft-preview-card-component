504a39be6cf4de2381262d4d0024f3cff71afa84
christianhelms <helmsmedia@gmail.com>
2022-03-04 22:55:13 -0600

    sanity check for css & dom

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h2>Hello, World!</h2>
+      <h2 className='text-red-400	'>Hello, World!</h2>
     </div>
   )
 }
