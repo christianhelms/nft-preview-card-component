4a0ca4f9832c24c808a2747b07717356a6066972
christianhelms <helmsmedia@gmail.com>
2022-03-04 23:50:44 -0600

    switched to normal img tag for svg display

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import Eth from "../public/icon-ethereum.svg";
-
 export default function Home() {
   return (
     <main>
@@ -19,7 +17,14 @@ export default function Home() {
           <p className='text-white font-outfit font-semibold text-2xl'>Equilibrium #3429</p>
           <p className='text-dark-gray font-outfit text-lg font-light'>Our Equilibrium collection promotes balance and calm.</p>
           <div>
-            <Eth />
+            <div>
+            <img src='/icon-ethereum.svg' alt='Eth' />
+            <p className='text-mint-blue font-outfit font-normal'>0.041 ETH</p>
+            </div>
+            <div>
+            <img src='/icon-clock.svg' alt='Eth' />
+            <p className='text-dark-gray font-outfit font-light'>3 days left</p>
+            </div>
           </div>
         </div>
       </div>
