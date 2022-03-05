cf45812b5ccca53c903685cd563d727dd16d06a5
christianhelms <helmsmedia@gmail.com>
2022-03-04 23:30:23 -0600

    image added

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,11 @@ export default function Home() {
       </Head>
       <div className='grid place-items-center h-screen bg-dark-blue'>
         <div>
-          <Image />
+          <Image 
+            src={'/image-equilibrium.jpg'}
+            width={400}
+            height={400}
+          />
           <p className='text-white font-outfit font-semibold text-2xl'>Equilibrium #3429</p>
           <p className='text-dark-gray font-outfit text-lg font-light'>Our Equilibrium collection promotes balance and calm.</p>
         </div>
